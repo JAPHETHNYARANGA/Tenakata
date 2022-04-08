108b3320d6953a25747aa4fe504c4faa2317cbcd
JAPHETHNYARANGA <nyaranga4@gmail.com>
2022-04-08 11:32:06 +0300

    Finished login with google xml and logic

--- app/src/main/java/com/storesoko/tenakata/LoginActivity.kt
+++ app/src/main/java/com/storesoko/tenakata/LoginActivity.kt
@@ -5,16 +5,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
 
 const val RC_SIGN_IN = 9001
 class LoginActivity : AppCompatActivity() {
@@ -33,6 +36,16 @@ class LoginActivity : AppCompatActivity() {
             .build()
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
+        sign_in_button.setSize(SignInButton.SIZE_WIDE)
+        sign_in_button.setOnClickListener {
+
+            signIn()
+
+            progressbar.visibility = View.VISIBLE
+
+        }
 
         auth = Firebase.auth
     }
@@ -63,14 +76,17 @@ class LoginActivity : AppCompatActivity() {
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "signInWithCredential:success")
+                    progressbar.visibility = View.INVISIBLE
                     val user = auth.currentUser
                    if(user != null){
                        startActivity(Intent(applicationContext, MainActivity::class.java))
+                       finish()
                    }
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
                     Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT).show()
+                    progressbar.visibility = View.INVISIBLE
                 }
             }
     }
