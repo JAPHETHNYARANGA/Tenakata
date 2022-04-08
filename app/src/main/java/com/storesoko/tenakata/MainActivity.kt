fcabadb9d58fe8dc31cf50931089119a90e0c0a5
JAPHETHNYARANGA <nyaranga4@gmail.com>
2022-04-08 11:42:11 +0300

    Finished the logout logic in the main Activity

--- app/src/main/java/com/storesoko/tenakata/MainActivity.kt
+++ app/src/main/java/com/storesoko/tenakata/MainActivity.kt
@@ -1,19 +1,44 @@
 package com.storesoko.tenakata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater :MenuInflater = menuInflater
         inflater.inflate(R.menu.top_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.logout ->{
+                logout()
+            }else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun logout(): Boolean {
+        Firebase.auth.signOut()
+        startActivity(Intent(applicationContext, LoginActivity::class.java))
+        finish()
         return true
     }
 }
