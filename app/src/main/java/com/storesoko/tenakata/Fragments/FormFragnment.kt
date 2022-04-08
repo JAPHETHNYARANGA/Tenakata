7dd1431fb8c2c9f7e0f706661e089c311b80d14c
JAPHETHNYARANGA <nyaranga4@gmail.com>
2022-04-08 22:15:54 +0300

    finished form validation for the registratio forms

--- app/src/main/java/com/storesoko/tenakata/Fragments/FormFragnment.kt
+++ app/src/main/java/com/storesoko/tenakata/Fragments/FormFragnment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.storesoko.tenakata.R
+import kotlinx.android.synthetic.main.fragment_form_fragnment.*
+import kotlinx.android.synthetic.main.fragment_form_fragnment.view.*
+import java.lang.Integer.parseInt
 
 
 class FormFragnment : Fragment() {
@@ -16,8 +20,51 @@ class FormFragnment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_form_fragnment, container, false)
+        val view = inflater.inflate(R.layout.fragment_form_fragnment, container, false)
+
+        view.submit.setOnClickListener{
+            validateForm()
+        }
+
+        return view
     }
+
+    private fun validateForm() {
+        val name = name.text.toString().trim()
+        val age = age.text.toString().trim()
+        val height = height.text.toString().trim()
+        val iqTest = iqTest.text.toString().trim()
+        val choosenLocation = choosenLocation.text.toString()
+
+        if(profile_image.drawable == null){
+            Toast.makeText(activity, "Please Take a Picture", Toast.LENGTH_SHORT).show()
+        }else if (name.isEmpty()){
+            layoutName.error = "please fill in your Name"
+        }else if (radioGroupGender.checkedRadioButtonId == -1){
+            Toast.makeText(activity, "Please select a gender", Toast.LENGTH_SHORT).show()
+        }else if(age.isEmpty()){
+            layoutAge.error= "please Insert your age"
+        }else if(radioGroupMaritalStatus.checkedRadioButtonId == -1){
+            Toast.makeText(activity, "Please select your marital status",Toast.LENGTH_SHORT).show()
+        }else if(height.isEmpty()){
+            layoutHeight.error = "Please enter your height"
+        }else if(iqTest.isEmpty()){
+            layoutIqTest.error = "Please Insert iq test"
+        }else if(parseInt(iqTest) < 100){
+            layoutIqTest.error = " your Iq Test must be above 100"
+        }else if(choosenLocation.isNullOrEmpty()){
+            Toast.makeText(activity,"Please choose a location", Toast.LENGTH_SHORT).show()
+        }else{
+
+            layoutName.isErrorEnabled = false
+            layoutAge.isErrorEnabled = false
+            layoutHeight.isErrorEnabled= false
+            layoutIqTest.isErrorEnabled = false
+            Toast.makeText(activity, "Everything is okay", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
 
 
 }
