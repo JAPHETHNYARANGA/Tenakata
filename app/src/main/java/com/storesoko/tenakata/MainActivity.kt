02bb3fe0a37a9c66586557807102d7854a04b51f
JAPHETHNYARANGA <nyaranga4@gmail.com>
2022-04-08 11:36:43 +0300

    added on create options menu to bind our menu file to the main Activity

--- app/src/main/java/com/storesoko/tenakata/MainActivity.kt
+++ app/src/main/java/com/storesoko/tenakata/MainActivity.kt
@@ -2,10 +2,18 @@ package com.storesoko.tenakata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater :MenuInflater = menuInflater
+        inflater.inflate(R.menu.top_menu, menu)
+        return true
     }
 }
